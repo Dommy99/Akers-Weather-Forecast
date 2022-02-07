5433037b7e248515a6bf059bf8d928863dc81d00
dominique <domin@DESKTOP-OT2687Q>
2022-02-07 16:30:35 -0600

    city data appened

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,10 @@ const key = '2c2f3bb9015dc7daada42b2297d77281'
 var searchBar = document.getElementById('search');
 var submitBtn = document.getElementById('submit');
 var gencity = document.getElementById('city');
+var tempEl = document.getElementById('temp');
+var windEl = document.getElementById('wind');
+var humidityEl = document.getElementById('humidity');
+var uvEl = document.getElementById('uv');
  
 submitBtn.addEventListener('click', function() {
     var city = searchBar.value;
@@ -24,19 +28,37 @@ submitBtn.addEventListener('click', function() {
 function findCity(city){
     limit = 5
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${key}`
+    
     fetch(url)
     .then(response =>{
         return response.json();
     }).then(data =>{
-        console.log(data);
         var currentCity = data[0].name;
         var currentCityLat = data[0].lat;
         var currentCityLon = data[0].lon;
-        console.log(currentCity, currentCityLat, currentCityLon);
         gencity.append(currentCity);
-    })
-   
+        getForcast(currentCityLat,currentCityLon);
+    });
+};
 
+function getForcast(currentCityLat, currentCityLon){
+
+    let cityForcast = `https://api.openweathermap.org/data/2.5/onecall?&units=imperial&lat=${currentCityLat}&lon=${currentCityLon}&appid=${key}`
+    fetch(cityForcast)
+    .then((response) =>{
+       return response.json(); 
+    }).then(data =>{
+        console.log(data);
+        var currentCity = data.current.temp;
+        var wind = data.current.wind_speed;
+        var humidity = data.current.humidity;
+        var uv = data.current.uvi;
+        tempEl.append(currentCity);
+        windEl.append(wind);
+        humidityEl.append(humidity);
+        uvEl.append(uv);
+        
+    })
 }
 
 // searchForm.addEventListener('submit', city);
